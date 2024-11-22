443d631431029ce8a07e7357139d87672e25b156
v a r r <91899516+VarrelKun@users.noreply.github.com>
2024-11-22 16:36:22 +0700

    Add files via upload

--- pages/api/downloader/igdl.js
+++ pages/api/downloader/igdl.js
@@ -1,28 +1,21 @@
 import axios from "axios"
 
 export default async function handler(req, res) {
-  let urls = req.query.urls
-  if (!urls) {
-    return res.status(400).json({
-      error: "Url Ig Nya Mana?"
-    })
-  }
-
- if (req.method !== "GET") {
+  if (req.method !== "GET") {
     return res.status(405).json({ error: "Method not allowed" })
   }
-
-  let url = `https://api.agatz.xyz/api/instagram?url=${urls}`
-
-  try {
-    const response = await axios.get(url)
-    const hasil = response.data.videoLinks.url[0]
-    res.status(200).json({
-      hasil
-    })
-  } catch (e) {
-    res.status(500).json({
-      error: "Ada masalah, coba lagi nanti"
-    })
+  const { url } = req.query
+  if (!url) {
+    return res.status(400).json({ error: q.msg.qUrl })
   }
+  const result = await igDl(url)
+  if (result.status === "error") {
+    return res.status(500).json(result)
+  }
+  res.status(200).json(result)
+}
+
+async function igDl(q) {
+  let response = await axios.get(`https://aemt.uk.to/download/igdl?url=${url}`)
+  return response.data
 }
