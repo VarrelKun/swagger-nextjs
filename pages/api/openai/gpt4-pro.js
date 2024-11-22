17d81af87b86e3e13a1a905996ca7dfd036648f4
v a r r <91899516+VarrelKun@users.noreply.github.com>
2024-11-22 16:30:21 +0700

    Add files via upload

--- pages/api/openai/gpt4-pro.js
+++ pages/api/openai/gpt4-pro.js
@@ -1,28 +1,21 @@
 import axios from "axios"
 
 export default async function handler(req, res) {
-  const { q } = req.query  
-  if (!q) {
-    return res.status(400).json({
-      error: "Text nya mana kocak?"
-    })
-  }
-
- if (req.method !== "GET") {
+  if (req.method !== "GET") {
     return res.status(405).json({ error: "Method not allowed" })
   }
-
-  let url = `https://aemt.uk.to/gpt4?text=${q}`
-
-  try {
-    const response = await axios.get(url)
-    const videoUrl = response.result
-    res.status(200).json({
-      videoUrl
-    })
-  } catch (e) {
-    res.status(500).json({
-      error: "Ada masalah, coba lagi nanti"
-    })
+  const { q } = req.query
+  if (!q) {
+    return res.status(400).json({ error: q.msg.qUrl })
   }
+  const result = await openAi(q)
+  if (result.status === "error") {
+    return res.status(500).json(result)
+  }
+  res.status(200).json(result)
+}
+
+async function openAi(q) {
+  let response = await axios.get(`https://aemt.uk.to/gpt4?text=${q}`)
+  return response.data
 }
