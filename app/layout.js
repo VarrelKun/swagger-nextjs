27d25b19563e21da99517aa3b627bb0764995876
v a r r <91899516+VarrelKun@users.noreply.github.com>
2024-11-22 17:05:55 +0700

    Add files via upload

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,29 @@
-// app/layout.js
-// https://nextjs.org/docs/app/getting-started/installation
+// app/docs/layout.js
+// custom
 
 export const metadata = {
-  title: "Comming Soon!",
-  description: "Comming Soon!",
+  title: "gopalasu - REST API Documentation",
+  description: "simple REST API created by Gopalasu !",
+  keywords: "Hello Word",
+  openGraph: {
+    title: "gopalasu - REST API Documentation",
+    description:
+      "docs.gopalasu.my.id is a free, simple REST API created by Gopalasu for the common good. Feel free to use it, but please avoid DDoS attacks.",
+    url: "https://docs.gopalasu.my.id",
+    type: "website",
+    images: [
+      {
+        url: "https://files.catbox.moe/7zjrpc.png",
+        width: 800,
+        height: 800,
+        alt: "gopal Logo",
+      },
+    ],
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
 }
 
 export default function RootLayout({ children }) {
